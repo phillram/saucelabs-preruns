d2978573b1ed2cd6ca598c84572bb7ce6e8136a7
Phill <14913130+phillram@users.noreply.github.com>
2019-10-23 22:44:12 -0700

    Create set_agent.sh
    Modifies the user agent for Safari on OSX.
    In this sample it will be read as "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3332.0 Safari/537.36"

---
+++ osx/set_agent.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+defaults write com.apple.Safari CustomUserAgent "\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3332.0 Safari/537.36\""
