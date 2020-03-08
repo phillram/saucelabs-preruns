0769634cd1cb4a2d6a6ac6d1bfdd47e975bad7e5
Phill <14913130+phillram@users.noreply.github.com>
2020-03-08 10:28:46 -0700

    Create macDownloadFiles.bash
    This prerun downloads the two image files in this repository and places them in /tmp/ folder.

---
+++ osx/macDownloadFiles.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o /tmp/cubone.png https://raw.githubusercontent.com/phillram/saucelabs-preruns/master/pictures/cubone.png
+
+curl -o /tmp/emolga.png https://raw.githubusercontent.com/phillram/saucelabs-preruns/master/pictures/emolga.png
