70b575cfdd289329b921ad324f7e5472ba709614
Phill <14913130+phillram@users.noreply.github.com>
2020-03-08 10:30:53 -0700

    Create downloadFiles.bat
    Downloads the two image files in this repository and places them on the desktop.

---
+++ windows/downloadFiles.bat
@@ -0,0 +1,4 @@
+@echo off
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/phillram/saucelabs-preruns/master/pictures/cubone.png C:\Users\Administrator\Desktop\cubone.png
+
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/phillram/saucelabs-preruns/master/pictures/emolga.png C:\Users\Administrator\Desktop\emolga.png
