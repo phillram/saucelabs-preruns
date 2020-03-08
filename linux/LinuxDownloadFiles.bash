942fb500ac58aa3603040a3536a56e607dd15f1a
Phill <14913130+phillram@users.noreply.github.com>
2020-03-08 10:27:10 -0700

    Create LinuxDownloadFiles.bash
    This prerun downloads the two image files in this repository and places them in /tmp/ folder.

---
+++ linux/LinuxDownloadFiles.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O /tmp/cubone.png https://raw.githubusercontent.com/phillram/saucelabs-preruns/master/pictures/cubone.png
+
+wget -O /tmp/emolga.png https://raw.githubusercontent.com/phillram/saucelabs-preruns/master/pictures/emolga.png
